803324dbb10dd14720de750004cfd2cfac61441f
Torsten Curdt <tcurdt@vafer.org>
2012-07-03 01:34:10 +0200

    more tests, better fifo impl

--- src/main/segments.c
+++ src/main/segments.c
@@ -6,29 +6,42 @@ static char* filename(const char *base_path, u_int32_t timestamp) {
   char *ret = malloc(len);
   if(!ret) return NULL;
   snprintf(ret, len - 1, "%s/%d", base_path, timestamp);
+  ret[len-1] = 0;
   return ret;
 }
+
+void segments_print(segments* segments) {
+  printf("segments: %d = { ", segments_size(segments));
+  segment* segment = segments->head;
+  while(segment != NULL) {
+    printf("%d:{ %d/%d } ", segment->timestamp, segment->size, segment->count);
+    segment = segment->next;
+  }
+  printf("}\n");
+}
+
 
 char* segments_segment_path(segments* segments, segment* segment) {
   return filename(segments->base_path, segment->timestamp);
 }
 
 int segments_init(segments* segments) {
+  segments->base_path = NULL;
   segments->head = NULL;
+  segments->tail = NULL;
   return OK;
 }
 
 int segments_destroy(segments* segments) {
-  if (segments->head) {
-    segment* curr = segments->head;
-    segment* next;
-    while(curr != NULL) {
-      next = curr->next;
-      free(curr);
-      curr = next;
-    }
-    segments->head = NULL;
+  segment* curr = segments->head;
+  segment* next;
+  while(curr != NULL) {
+    next = curr->next;
+    free(curr);
+    curr = next;
   }
+  segments->head = NULL;
+  segments->tail = NULL;
   return OK;
 }
 
@@ -57,43 +70,36 @@ u_int32_t segments_count(segments* segments) {
 }
 
 segment* segments_pop(segments* segments) {
-
   segment* head = segments->head;
-  segment* prev = NULL;
-  segment* curr = head;
-  while(curr != NULL && curr->next != NULL) {
-    prev = curr;
-    curr = curr->next;
+  if (head) {
+    segments->head = head->next;
+    // set tail to null if last element
+    if (segments->head == NULL) {
+      segments->tail = NULL;
+    }
   }
-
-  if (prev) {
-    // popping an element from the chain
-    prev->next = NULL;
-  } else {
-    // popping the last element
-    segments->head = NULL;
-  }
-
-  return curr;
+  return head;
 }
 
 int segments_add(segments* segments, u_int32_t timestamp) {
 
   // continously increasing
-  if (segments->head) timestamp = MAX(timestamp, segments->head->timestamp + 1);
+  if (segments->tail) timestamp = MAX(timestamp, segments->tail->timestamp + 1);
 
-  char* path = filename(segments->base_path, timestamp);
-  check(path, OOM);
+  u_int32_t size = 0;
 
   // check length
-  u_int32_t size = 0;
-  struct stat st;
-  if (stat(path, &st) == OK) {
-    check(S_ISREG(st.st_mode), "not a regular file %s", path);
-    size = st.st_size;
-    // FIXME maybe check a magic header?
+  if (segments->base_path) {
+    char* path = filename(segments->base_path, timestamp);
+    check(path, OOM);
+    struct stat st;
+    if (stat(path, &st) == OK) {
+      check(S_ISREG(st.st_mode), "not a regular file %s", path);
+      size = st.st_size;
+      // FIXME maybe check a magic header?
+    }
+    free(path);
   }
-  free(path);
 
   segment* new_segment = malloc(sizeof(segment));
   check(new_segment, OOM);
@@ -104,7 +110,7 @@ int segments_add(segments* segments, u_int32_t timestamp) {
   segment* head = segments->head;
   segment* prev = NULL;
   segment* curr = head;
-  while(curr != NULL && new_segment->timestamp < curr->timestamp) {
+  while(curr != NULL && new_segment->timestamp > curr->timestamp) {
     prev = curr;
     curr = curr->next;
   }
@@ -117,6 +123,10 @@ int segments_add(segments* segments, u_int32_t timestamp) {
     // new segments head
     new_segment->next = head;
     segments->head = new_segment;
+  }
+
+  if (new_segment->next == NULL) {
+    segments->tail = new_segment;
   }
 
   return OK;

9705a1f1cf00ba5aeb6f3b947f195d3d627ac0e5
Torsten Curdt <>
2012-07-11 18:27:19 +0000

    build on linux

--- src/main/unixy.c
+++ src/main/unixy.c
@@ -1,43 +1,17 @@
+#define _XOPEN_SOURCE 700 // http://stackoverflow.com/questions/782338/warning-with-nftw
+
 #include "unixy.h"
 #include "common.h"
 
 #include <ftw.h>
+#include <stdlib.h>
 #include <fcntl.h>
-// #define _XOPEN_SOURCE 500
-
-// #include <fts.h>
-// #include <string.h>
 
 const char* tmp_create() {
   char *path = mkdtemp(strdup("/tmp/orbs-XXXXXX"));
   assert(path);
   return path;
 }
-
-// int tmp_remove(const char* path) {
-//   if (path == NULL) return ERROR;
-// 
-//   char* const paths[] = { (char*)path, NULL };
-// 
-//   FTS *tree = fts_open(paths, FTS_NOCHDIR, 0);
-//   check(tree, "fts_open");
-// 
-//   FTSENT *node;
-//   while ((node = fts_read(tree))) {
-//     if (node->fts_level > 0 && node->fts_name[0] == '.') {
-//       fts_set(tree, node, FTS_SKIP);
-//     } else if (node->fts_info & FTS_F) {
-//       check(remove(node->fts_path) == OK, "failed to remove file %s", node->fts_path);
-//     } else if (node->fts_info & FTS_DP) {
-//       check(remove(node->fts_path) == OK, "failed to remove dir %s", node->fts_path);
-//     }
-//   }
-// 
-//   check(fts_close(tree) == OK, "fts_close");
-// 
-//   free((void*)path);
-//   return OK;
-// }
 
 static int tmp_remove_file(const char* fpath, const struct stat* sb, int typeflag, struct FTW* ftwbuf) {
   UNUSED(sb);
